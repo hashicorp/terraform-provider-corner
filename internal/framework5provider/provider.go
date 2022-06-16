d2b5e52ea61d3a0c7de57ce1ad9c115a510fb9a2
Brian Flad <bflad417@gmail.com>
2022-06-16 08:12:18 -0400

    internal/frameworkprovider: Add acceptance test for protocol version 5 support (#70)

---
+++ internal/framework5provider/provider.go
@@ -0,0 +1,50 @@
+package framework
+
+import (
+	"context"
+	"os"
+
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-provider-corner/internal/backend"
+)
+
+var stderr = os.Stderr
+
+func New() tfsdk.Provider {
+	return &provider{}
+}
+
+type provider struct {
+	client *backend.Client
+}
+
+func (p *provider) GetSchema(_ context.Context) (tfsdk.Schema, diag.Diagnostics) {
+	return tfsdk.Schema{
+		Attributes: map[string]tfsdk.Attribute{
+			"dummy": {
+				Type:     types.StringType,
+				Optional: true,
+			},
+		},
+	}, nil
+}
+
+func (p *provider) Configure(ctx context.Context, req tfsdk.ConfigureProviderRequest, resp *tfsdk.ConfigureProviderResponse) {
+	client, err := backend.NewClient()
+	if err != nil {
+		resp.Diagnostics.AddError("Error initialising client", err.Error())
+	}
+	p.client = client
+}
+
+func (p *provider) GetResources(_ context.Context) (map[string]tfsdk.ResourceType, diag.Diagnostics) {
+	return map[string]tfsdk.ResourceType{
+		"framework_user": resourceUserType{},
+	}, nil
+}
+
+func (p *provider) GetDataSources(_ context.Context) (map[string]tfsdk.DataSourceType, diag.Diagnostics) {
+	return map[string]tfsdk.DataSourceType{}, nil
+}
