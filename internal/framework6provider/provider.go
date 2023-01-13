abe0a01e9d112ea098683302b5d3a1ae70b32aeb
Brian Flad <bflad417@gmail.com>
2023-01-13 16:18:13 -0500

    internal/framework(5|6)provider: Smoke testing for all attribute and block types
    Reference: https://github.com/hashicorp/terraform-plugin-framework/issues/623
    
    This sort of end-to-end testing would have caught the data handling issue before the framework v1.1.0 release:
    
    Running the testing with framework v1.1.0:
    
    ```
    === RUN   TestSchemaResource_basic
    panic: tftypes.NewValue can't use []tftypes.Value as a tftypes.Object; expected types are: map[string]tftypes.Value
    
    goroutine 2659 [running]:
    github.com/hashicorp/terraform-plugin-go/tftypes.NewValue(...)
            /Users/bflad/go/pkg/mod/github.com/hashicorp/terraform-plugin-go@v0.14.3/tftypes/value.go:273
    github.com/hashicorp/terraform-plugin-framework/internal/fwschemadata.(*Data).ReifyNullCollectionBlocks.func1(0x101922568?, {{0x101922510?, 0x140002824e0?}, {0x0?, 0x0?}})
    ```

--- internal/framework6provider/provider.go
+++ internal/framework6provider/provider.go
@@ -43,6 +43,7 @@ func (p *testProvider) Configure(ctx context.Context, req provider.ConfigureRequ
 
 func (p *testProvider) Resources(_ context.Context) []func() resource.Resource {
 	return []func() resource.Resource{
+		NewSchemaResource,
 		NewUserResource,
 	}
 }
