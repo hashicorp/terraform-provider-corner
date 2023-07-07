624556b99fab4025e389d986b84fc8640e67112e
Brian Flad <bflad417@gmail.com>
2023-07-07 09:16:55 -0400

    internal/framework5provider: Fix timeouts testing for protocol version 5

--- internal/framework5provider/timeouts_resource_test.go
+++ internal/framework5provider/timeouts_resource_test.go
@@ -7,14 +7,14 @@ import (
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
-	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
 func TestTimeoutsResource_unconfigured(t *testing.T) {
 	resource.UnitTest(t, resource.TestCase{
-		ProtoV6ProviderFactories: map[string]func() (tfprotov6.ProviderServer, error){
-			"framework": providerserver.NewProtocol6WithError(New()),
+		ProtoV5ProviderFactories: map[string]func() (tfprotov5.ProviderServer, error){
+			"framework": providerserver.NewProtocol5WithError(New()),
 		},
 		Steps: []resource.TestStep{
 			{
@@ -30,8 +30,8 @@ func TestTimeoutsResource_unconfigured(t *testing.T) {
 
 func TestTimeoutsResource_configured(t *testing.T) {
 	resource.UnitTest(t, resource.TestCase{
-		ProtoV6ProviderFactories: map[string]func() (tfprotov6.ProviderServer, error){
-			"framework": providerserver.NewProtocol6WithError(New()),
+		ProtoV5ProviderFactories: map[string]func() (tfprotov5.ProviderServer, error){
+			"framework": providerserver.NewProtocol5WithError(New()),
 		},
 		Steps: []resource.TestStep{
 			{
