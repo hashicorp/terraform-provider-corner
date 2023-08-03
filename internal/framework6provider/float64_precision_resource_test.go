84917f9d21184c5c2268bf39165642c6bd85ba20
Austin Valle <austinvalle@gmail.com>
2023-08-03 09:46:52 -0400

    add plancheck test

--- internal/framework6provider/float64_precision_resource_test.go
+++ internal/framework6provider/float64_precision_resource_test.go
@@ -9,8 +9,10 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/plancheck"
 )
 
+// This test verifies that the correct precision value is stored in state
 // https://github.com/hashicorp/terraform-plugin-framework/issues/815
 func TestSchemaResource_Float64Attribute_Precision(t *testing.T) {
 	resource.UnitTest(t, resource.TestCase{
@@ -32,6 +34,39 @@ func TestSchemaResource_Float64Attribute_Precision(t *testing.T) {
 				}`,
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("framework_float64_precision.test", "float64_attribute", "0.020000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000006"),
+				),
+			},
+		},
+	})
+}
+
+// This test verifies that a plan is detected when encountering a more precise value
+// https://github.com/hashicorp/terraform-plugin-framework/issues/815
+func TestSchemaResource_Float64Attribute_Precision_Plan(t *testing.T) {
+	resource.UnitTest(t, resource.TestCase{
+		ProtoV6ProviderFactories: map[string]func() (tfprotov6.ProviderServer, error){
+			"framework": providerserver.NewProtocol6WithError(New()),
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: `resource "framework_float64_precision" "test" {
+					float64_attribute = 0.01
+				}`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("framework_float64_precision.test", "float64_attribute", "0.01"),
+				),
+			},
+			{
+				Config: `resource "framework_float64_precision" "test" {
+					float64_attribute = 1 - 0.99
+				}`,
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectNonEmptyPlan(),
+					},
+				},
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("framework_float64_precision.test", "float64_attribute", "0.010000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000003"),
 				),
 			},
 		},
