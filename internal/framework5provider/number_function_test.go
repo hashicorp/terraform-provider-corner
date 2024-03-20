97e64cc3d77ed891dba318bc1b2daea983856f30
Benjamin Bennett <ben.bennett@hashicorp.com>
2024-03-20 11:35:13 +0000

    Updating number test

--- internal/framework5provider/number_function_test.go
+++ internal/framework5provider/number_function_test.go
@@ -20,12 +20,10 @@ import (
 
 func TestNumberFunction_known(t *testing.T) {
 	// Using 9223372036854775808, the smallest number that can't be represented as an int64,
-	// results in an Terraform error where [Planned value does not match config value for cty.NumberIntVal],
-	// which is related to a bug in go.cty relating to [Large integer comparisons and msgpack encoding].
+	// results in an Terraform error where [Planned value does not match config value for number].
 	// A value of 9223372036854775809 is used for the meanwhile.
 	//
-	// [Planned value does not match config value for cty.NumberIntVal]: https://github.com/hashicorp/terraform/issues/34589
-	// [Large integer comparisons and msgpack encoding]: https://github.com/zclconf/go-cty/pull/176
+	// [Planned value does not match config value for number]: https://github.com/hashicorp/terraform/issues/34866
 	bf, _, err := big.ParseFloat("9223372036854775809", 10, 512, big.ToNearestEven)
 
 	if err != nil {
