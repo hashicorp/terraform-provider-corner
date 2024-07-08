bbbb4778878e023f05841dcb4c36eef53421f9f4
Selena Goods <github@simplebox.anonaddy.com>
2024-07-08 17:04:42 -0400

    Add `MaxFloat32` precision tests for `float64_precision_resource` and create separate tests for Terraform `v1.9.0` and above

--- internal/framework6provider/float32_precision_resource_test.go
+++ internal/framework6provider/float32_precision_resource_test.go
@@ -7,10 +7,12 @@ import (
 	"regexp"
 	"testing"
 
+	"github.com/hashicorp/go-version"
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/plancheck"
+	"github.com/hashicorp/terraform-plugin-testing/tfversion"
 )
 
 // This test verifies that the correct precision value is stored in state
@@ -77,13 +79,24 @@ func TestSchemaResource_Float32Attribute_Precision_Plan(t *testing.T) {
 // This test documents edge case behavior with math.MaxFloat32
 // This number is unique in that it has an exact representation in both float32 and float64,
 // it is also an integer, which may affect how this value gets encoded/decoded with go-cty.
+// https://github.com/hashicorp/terraform-plugin-framework/issues/1017
 func TestSchemaResource_Float32Attribute_Precision_MaxFloat32(t *testing.T) {
 	resource.UnitTest(t, resource.TestCase{
+		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+			// go-cty v1.14.4 uses string msgpack encoding instead of float msgpack encoding for large whole numbers
+			// https://github.com/hashicorp/terraform/commit/cd252557e4200e031b942d9ff35c455bb30d858f
+			// This changes allows the math.MaxFloat32 value to succeed in planning but fail during apply.
+			// Terraform v1.9.0-alpha20240501 is the first Terraform version to use this updated encoding.
+			tfversion.All(
+				tfversion.SkipBelow(tfversion.Version0_15_0),
+				tfversion.SkipAbove(version.Must(version.NewVersion("1.9.0-alpha20240501"))),
+			),
+		},
 		ProtoV6ProviderFactories: map[string]func() (tfprotov6.ProviderServer, error){
 			"framework": providerserver.NewProtocol6WithError(New()),
 		},
 		Steps: []resource.TestStep{
-			// Error when planned with 64-bit precision
+			// Error when planned with full precision
 			{
 				Config: `resource "framework_float32_precision" "test" {
 					float32_attribute = 340282346638528859811704183484516925440
@@ -93,7 +106,7 @@ func TestSchemaResource_Float32Attribute_Precision_MaxFloat32(t *testing.T) {
 				),
 				ExpectError: regexp.MustCompile(`.*planned value\s{0,10}cty\.NumberIntVal\(3\.4028234663852886e\+38\) does not match config value\s{0,10}cty\.NumberIntVal\(3\.4028234663852885981170418348451692544e\+38\)`),
 			},
-			// Error when planned with 64-bit precision (scientific notation)
+			// Error when planned with full precision (scientific notation)
 			{
 				Config: `resource "framework_float32_precision" "test" {
 					float32_attribute = 3.40282346638528859811704183484516925440e+38
@@ -103,7 +116,7 @@ func TestSchemaResource_Float32Attribute_Precision_MaxFloat32(t *testing.T) {
 				),
 				ExpectError: regexp.MustCompile(`.*planned value\s{0,10}cty\.NumberIntVal\(3\.4028234663852886e\+38\) does not match config value\s{0,10}cty\.NumberIntVal\(3\.4028234663852885981170418348451692544e\+38\)`),
 			},
-			// No error when planned with 32-bit precision
+			// No error when planned with 53-bit precision
 			{
 				Config: `resource "framework_float32_precision" "test" {
 					float32_attribute = 340282346638528860000000000000000000000
@@ -112,7 +125,7 @@ func TestSchemaResource_Float32Attribute_Precision_MaxFloat32(t *testing.T) {
 					resource.TestCheckResourceAttr("framework_float32_precision.test", "float32_attribute", "340282346638528860000000000000000000000"),
 				),
 			},
-			// Semantic equality with 32-bit scientific notation representation
+			// Semantic equality with 53-bit precision scientific notation representation
 			{
 				Config: `resource "framework_float32_precision" "test" {
 					float32_attribute = 3.4028234663852886e+38
@@ -126,7 +139,7 @@ func TestSchemaResource_Float32Attribute_Precision_MaxFloat32(t *testing.T) {
 					resource.TestCheckResourceAttr("framework_float32_precision.test", "float32_attribute", "340282346638528860000000000000000000000"),
 				),
 			},
-			// No plan is detected when 32-bit value is replaced with 64-bit value
+			// No plan is detected when 53-bit precision value is replaced with higher precision value
 			{
 				Config: `resource "framework_float32_precision" "test" {
 					float32_attribute = 3.40282346638528859811704183484516925440e+38
@@ -144,51 +157,78 @@ func TestSchemaResource_Float32Attribute_Precision_MaxFloat32(t *testing.T) {
 	})
 }
 
-func TestSchemaResource_Float32Attribute_Precision_Overflow_Underflow(t *testing.T) {
+// This test documents edge case behavior with math.MaxFloat32 with Terraform v1.9.0 and above
+// This number is unique in that it has an exact representation in both float32 and float64,
+// it is also an integer, which may affect how this value gets encoded/decoded with go-cty.
+// https://github.com/hashicorp/terraform-plugin-framework/issues/1017
+//
+// go-cty v1.14.4 uses string msgpack encoding instead of float msgpack encoding for large whole numbers
+// https://github.com/hashicorp/terraform/commit/cd252557e4200e031b942d9ff35c455bb30d858f
+// This changes allows the math.MaxFloat32 value to succeed in planning but fail during apply.
+// Terraform v1.9.0-alpha20240501 is the first Terraform version to use this updated encoding.
+func TestSchemaResource_Float32Attribute_Precision_MaxFloat32_TF1_9(t *testing.T) {
 	resource.UnitTest(t, resource.TestCase{
+		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+			tfversion.SkipBelow(version.Must(version.NewVersion("1.9.0-alpha20240501"))),
+		},
 		ProtoV6ProviderFactories: map[string]func() (tfprotov6.ProviderServer, error){
 			"framework": providerserver.NewProtocol6WithError(New()),
 		},
 		Steps: []resource.TestStep{
-			// float32 overflow
+			// Error when planned with full precision
 			{
 				Config: `resource "framework_float32_precision" "test" {
-					float32_attribute = 3.40282346638528859811704183484516925440e+39
+					float32_attribute = 340282346638528859811704183484516925440
 				}`,
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("framework_float32_precision.test", "float32_attribute", "340282346638528859811704183484516925440"),
 				),
-				ExpectError: regexp.MustCompile(`.*Error: Value %!s\(\*big\.Float=3\.402823466e\+39\) cannot be represented as a\s{0,10}32-bit floating point.`),
+				ExpectError: regexp.MustCompile(`.*Error: Provider produced inconsistent result after apply`),
 			},
-			// float32 negative overflow
+			// Error when planned with full precision (scientific notation)
 			{
 				Config: `resource "framework_float32_precision" "test" {
-					float32_attribute = -3.40282346638528859811704183484516925440e+39
+					float32_attribute = 3.40282346638528859811704183484516925440e+38
 				}`,
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("framework_float32_precision.test", "float32_attribute", "340282346638528859811704183484516925440"),
 				),
-				ExpectError: regexp.MustCompile(`.*Error: Value %!s\(\*big\.Float=-3\.402823466e\+39\) cannot be represented as a\s{0,10}32-bit floating point.`),
+				ExpectError: regexp.MustCompile(`.*Error: Provider produced inconsistent result after apply`),
 			},
-			// float32 underflow
+			// No error when planned with 53-bit precision
 			{
 				Config: `resource "framework_float32_precision" "test" {
-					float32_attribute = 1.401298464324817070923729583289916131280e-46
+					float32_attribute = 340282346638528860000000000000000000000
 				}`,
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("framework_float32_precision.test", "float32_attribute", "340282346638528859811704183484516925440"),
+					resource.TestCheckResourceAttr("framework_float32_precision.test", "float32_attribute", "340282346638528860000000000000000000000"),
 				),
-				ExpectError: regexp.MustCompile(`.*Error: Value %!s\(\*big\.Float=1\.401298464e-46\) cannot be represented as a\s{0,10}32-bit floating point.`),
 			},
-			// float32 negative underflow
+			// Semantic equality with 53-bit precision scientific notation representation
 			{
 				Config: `resource "framework_float32_precision" "test" {
-					float32_attribute = -1.401298464324817070923729583289916131280e-46
+					float32_attribute = 3.4028234663852886e+38
 				}`,
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectEmptyPlan(),
+					},
+				},
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("framework_float32_precision.test", "float32_attribute", "340282346638528859811704183484516925440"),
+					resource.TestCheckResourceAttr("framework_float32_precision.test", "float32_attribute", "340282346638528860000000000000000000000"),
 				),
-				ExpectError: regexp.MustCompile(`.*Error: Value %!s\(\*big\.Float=-1\.401298464e-46\) cannot be represented as a\s{0,10}32-bit floating point.`),
+			},
+			// Resource plans an update when 53-bit precision value is replaced with higher precision value but fails during apply
+			{
+				Config: `resource "framework_float32_precision" "test" {
+					float32_attribute = 3.40282346638528859811704183484516925440e+38
+				}`,
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction("framework_float32_precision.test", plancheck.ResourceActionUpdate),
+					},
+				},
+				ExpectError: regexp.MustCompile(`.*Error: Provider produced inconsistent result after apply`),
 			},
 		},
 	})
