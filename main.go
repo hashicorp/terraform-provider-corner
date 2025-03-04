bd0ac79ef0ec5b4797b23249df8a2dbc1b06dbe6
Austin Valle <austinvalle@gmail.com>
2025-03-04 14:46:45 -0500

    clean up the main.go

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ package main
 
 import (
 	"context"
-	"flag"
 	"log"
 
 	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
@@ -16,17 +15,20 @@ import (
 	sdkv2 "github.com/hashicorp/terraform-provider-corner/internal/sdkv2provider"
 )
 
+// MAINTAINER NOTE: The current main function does not include all of the available corner resource types
+// as the corner provider is rarely needed to be built as a single binary. The corner CI testing suite executes
+// Go test directly against the internal packages which contain provider servers that have conflicting type names
+// and different provider namespaces (testing both v5 and v6 protocols). Debugging those provider servers can also
+// be achieved with Go's testing tools and a debugger such as delve.
+//
+// In the future, if we want to adjust this provider to be built as a single binary, we will need to refactor all of the
+// internal provider packages and resource type names to avoid conflicts, as well as allow the provider binary to be built
+// with protocol v5 or v6 conditionally.
 func main() {
 	ctx := context.Background()
-
-	var debug bool
-
-	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
-	flag.Parse()
-
 	providers := []func() tfprotov5.ProviderServer{
 		func() tfprotov5.ProviderServer {
-			return protocol.Server(true)
+			return protocol.Server(false)
 		},
 		sdkv2.New().GRPCProvider,
 	}
@@ -37,12 +39,7 @@ func main() {
 		log.Fatalf("unable to create provider: %s", err)
 	}
 
-	if debug {
-		err = tf5server.Serve("registry.terraform.io/hashicorp/corner", muxServer.ProviderServer, tf5server.WithManagedDebug())
-	} else {
-		err = tf5server.Serve("registry.terraform.io/hashicorp/corner", muxServer.ProviderServer)
-	}
-
+	err = tf5server.Serve("registry.terraform.io/hashicorp/corner", muxServer.ProviderServer)
 	if err != nil {
 		log.Fatalf("unable to serve provider: %s", err)
 	}
