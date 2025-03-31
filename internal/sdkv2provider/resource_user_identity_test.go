664aa0dafb06d7f7c8fdc1262c9a1fc306b8bcb6
Austin Valle <austinvalle@gmail.com>
2025-03-31 14:59:34 -0400

    add version check to sdkv2 test

--- internal/sdkv2provider/resource_user_identity_test.go
+++ internal/sdkv2provider/resource_user_identity_test.go
@@ -6,14 +6,21 @@ package sdkv2
 import (
 	"testing"
 
+	"github.com/hashicorp/go-version"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
 	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+	"github.com/hashicorp/terraform-plugin-testing/tfversion"
 )
 
 func testAccResourceUserIdentity(t *testing.T) resource.TestCase {
 	return resource.TestCase{
-		PreCheck:  func() { testAccPreCheck(t) },
+		PreCheck: func() { testAccPreCheck(t) },
+		// Latest alpha version that this JSON data is available in
+		// https://github.com/hashicorp/terraform/releases/tag/v1.12.0-alpha20250319
+		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+			tfversion.SkipBelow(version.Must(version.NewVersion("1.12.0-alpha20250319"))),
+		},
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
